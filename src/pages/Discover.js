44df4a7de1c5946409dc0beef05a9f62cc85397d
mamun8893 <0316361936@student.wub.edu.bd>
2022-01-07 23:43:04 +0600

    redux

---
+++ src/pages/Discover.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Book from "../components/Book/Book";
+import books from "../fakeData/books.json";
+import PageLayout from "../components/PageLayout/PageLayout";
+import { useSelector } from "react-redux";
+const Discover = () => {
+  const discover = useSelector((state) => state.books.discover);
+  return (
+    <PageLayout>
+      {discover.map((book) => (
+        <Book key={book.id} book={book} />
+      ))}
+    </PageLayout>
+  );
+};
+
+export default Discover;
